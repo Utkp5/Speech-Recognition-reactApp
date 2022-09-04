2bbebbcc59ff06507c0234cdbbd065e068492d6d
Utkp5 <utkp200@gmail.com>
2022-09-04 17:57:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { MdMicNone } from "react-icons/md";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <textarea name="" id="text" cols="30" rows="10"></textarea>
+        <button class="btn" type="button">
+            <i><MdMicNone /></i>
+        </button>
+        <span id="action"></span>
+    </div>    
   );
 }
 
