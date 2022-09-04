d1b46217325ffb60f8d107a2139ae37665aa2962
Utkp5 <utkp200@gmail.com>
2022-09-04 19:01:56 +0530

    function update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import './App.css';
-import { MdMicNone } from "react-icons/md";
+import { MdMic } from "react-icons/md";
+import { MdMicOff } from "react-icons/md";
+import { MdFavorite } from "react-icons/md";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { useRef, useState } from 'react';
 
 
 function App() {
+
+  const refElement = useRef("");
+  const refEle = useRef("");
+  // const [listen,setlisten] = useState(SpeechRecognition);
+
+  const {transcript} = useSpeechRecognition();
+
+
+  function Handleclickon() {
+    SpeechRecognition.startListening();
+      console.log("We are listening. Try speaking into the microphone.");
+      refElement.current.innerText = '' ;
+      refEle.current.innerText = "Listening, please speak...";
+  }
+
+  function Handleclickoff() {
+    SpeechRecognition.stopListening();
+    console.log("Stopped listening, hope you are done...");
+    refEle.current.innerText = "Stopped listening, hope you are done...";
+    refElement.current.innerText = transcript
+  }
+
   return (
-    <div>
-    <textarea name="" id="text" cols="30" rows="10"></textarea>
-        <button class="btn" type="button">
-            <i><MdMicNone /></i>
-        </button>
-        <span id="action"></span>
+    <div className='main'>
+    <textarea name="" id="text" cols="30" rows="10"  ref={refElement}></textarea>
+      <div className='buttn_div'>
+        <button className="btn" type="button" onClick={Handleclickon}><i><MdMic size={26}/></i></button>
+        <button className="btn" type="button" onClick={Handleclickoff}><i><MdMicOff size={26}/></i></button>
+      </div>
+    <span id="action" ref={refEle}><MdFavorite color="red"/></span>
     </div>    
   );
 }
